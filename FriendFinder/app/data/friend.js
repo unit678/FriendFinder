b594f2a254397a3f82acaf58967a0a00970aee46
Garrion Martin <unit678@gmail.com>
2018-11-17 21:00:32 -0500

    final

---
+++ FriendFinder/app/data/friend.js
@@ -0,0 +1,102 @@
+var friendList = [
+
+
+// Note how we export the array. This makes it accessible to other files using require.
+    {
+    "name":"Ahmed Johnson",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+    5,
+    1,
+    4,
+    5,
+    5,
+    1,
+    3,
+    1,
+    3,
+    5
+        ]
+
+},
+    {
+        "name":"Mike Miller",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            2,
+            4,
+            3,
+            1,
+            3,
+            5,
+            3,
+            1,
+            4,
+            3
+
+        ]
+
+    },
+
+    {
+        "name":"Jordan Lee",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            3,
+            2,
+            4,
+            4,
+            4,
+            1,
+            2,
+            2,
+            4,
+            2
+
+        ]
+
+
+    },
+
+    {
+        "name":"Reggie Miller",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,
+            3,
+            5,
+            2,
+            5,
+            1,
+            3,
+            5,
+            4,
+            5
+        ]
+
+    },
+
+    {
+        "name":"Ray Allen",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            2,
+            3,
+            4,
+            4,
+            2,
+            1,
+            4,
+            5,
+            4,
+            2
+
+        ]
+
+
+    },
+
+
+
+];
+module.exports = friendList;
